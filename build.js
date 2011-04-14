6f1d246bdd6e9f8deeef1f98d41715f314168f43
Renat Zubairov <renat.zubairov@gmail.com>
2011-04-14 17:08:26 +0200

    Improved comments, added comman line option handling

--- build.js
+++ build.js
@@ -1,3 +1,14 @@
+/**
+*	A maven parallelization tool for building UN/EDIFACT directories
+*
+*	To start it please make sure you have a node.js installed
+*	
+*		%> node build.js [maven parameters]
+*
+* 	In case no maven parameters are specified, '-o clean install' will be used.
+* 	Standard output of forked child processes is stored under log directory
+* 
+**/
 var util   = require('util'),
 	path = require('path'),
 	sys = require('sys'),
@@ -6,15 +17,18 @@ var util   = require('util'),
     exec = require('child_process').exec,
     async = require('async');
 
+var cmdParams = process.argv.slice(2);
+var MAVEN_PARAMETERS = cmdParams.length ? cmdParams : ['-o', 'clean', 'install'];
+
 // Run maven
 var mvn = function(cwd, cb) {
 	path.exists(cwd, function(exists) {
 		if (exists) {
 			sys.puts('Starting  mvn ' + cwd);
 			var procName = cwd.match('/(d[0-9]+./[a-z]+)')[1].replace('/','_');
-			var logFile = process.cwd() + '/log/' + procName + '.log';
+			var logFile = process.cwd() + '/logs/' + procName + '.log';
 			var logStream = fs.createWriteStream(logFile);
-			var proc = spawn('mvn', ['-o', 'clean'], {"cwd" : cwd });
+			var proc = spawn('mvn', MAVEN_PARAMETERS , {"cwd" : cwd });
 			proc.stdout.pipe(logStream);
 			proc.on('exit', function(code) {
 				sys.puts('Completed mvn ' + cwd);
@@ -38,17 +52,18 @@ var buildDir = function(directory, callback) {
 			sys.puts('ERROR Happened when processing directory ' + directory);
 		}
 		callback();
- 		console.log('Build for :', root, 'took\t\t', new Date() - start, 'ms');
+ 		console.log('Build for :', root, 'took\t\t', Math.round((new Date() - start) / 1000), 's');
 	});
 }
 
 // Here the program starts
 var dirs = [], 
 	cpus = require('os').cpus().length - 1,
-	logDir = process.cwd() + '/log';
+	logDir = process.cwd() + '/logs';
 
 sys.puts('Starting a build process with ' + cpus + ' parallel threads');
 sys.puts('Logs are stored in ' + logDir);
+console.log('Launching maven parametrers "%s"', MAVEN_PARAMETERS.join(' '));
 exec('rm -rf ' + logDir, function(error) {
 	// Let's create a log directory
 	fs.mkdirSync(logDir, 0776);
